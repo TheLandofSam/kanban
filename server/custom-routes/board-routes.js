59a7d82e6d0e653c86372bc2acae9c5ddb611616
Samantha Tanner <samz_shadow@yahoo.com>
2017-05-24 14:20:01 -0600

    merge issues

--- server/custom-routes/board-routes.js
+++ server/custom-routes/board-routes.js
@@ -4,26 +4,26 @@ let Tasks = require('../models/task')
 let Comments = require('../models/comment')
 
 export default {
-  getAll:{
-    path: '/boards/:boardId/lists/:listId/tasks/:taskId/comments',
-    reqType: 'get',
-    method(req, res, next){
-      let action = 'Get All'
-      Lists.find({boardId: req.params.boardId})
-        .then(lists => {
-          lists.forEach(list=>{
-            Tasks.find({listId: list._id})
-            .then(tasks => {
-              Comments.findById({taskId: task._id})          
-        })
-          res.send(handleResponse(action, lists))
-        })
-        })
-        .catch(error => {
-          return next(handleResponse(action, null, error))
-        })
-    }
-  },
+  // getAll:{
+  //   path: '/boards/:boardId/lists/:listId/tasks/:taskId/comments',
+  //   reqType: 'get',
+  //   method(req, res, next){
+  //     let action = 'Get All'
+  //     Lists.find({boardId: req.params.boardId})
+  //       .then(lists => {
+  //         lists.forEach(list=>{
+  //           Tasks.find({listId: list._id})
+  //           .then(tasks => {
+  //             Comments.findById({taskId: task._id})          
+  //       })
+  //         res.send(handleResponse(action, lists))
+  //       })
+  //       })
+  //       .catch(error => {
+  //         return next(handleResponse(action, null, error))
+  //       })
+  //   }
+  // },
   getListsByBoardId:{ 
     path: '/boards/:boardId/lists/', //: before wordmeans it is a variable
     reqType: 'get',
