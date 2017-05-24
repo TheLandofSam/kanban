57ee290f5328cf5754b9d4a51a9729eff2336c19
Samantha Tanner <samz_shadow@yahoo.com>
2017-05-24 14:04:48 -0600

    more route work

--- server/custom-routes/board-routes.js
+++ server/custom-routes/board-routes.js
@@ -24,39 +24,94 @@ export default {
         })
     }
   },
-  getLists:{ 
-    path: '/boards/:boardId/lists/',
+  getListsByBoardId:{ 
+    path: '/boards/:boardId/lists/', //: before wordmeans it is a variable
     reqType: 'get',
     method(req, res, next){
       let action = 'Get Lists'
-      Lists.find({boardId: req.params.boardId})
-        .then(lists => {
-          res.send(handleResponse(action, lists))
+      Boards.findById(req.params.boardId) //({boardId: req.params.boardId}) this says find me all objs where this is true, above says return all...
+        .then(board => { //same as .then(function(board)){
+          Lists.find({boardId: req.params.boardId})
+          .then(lists=>{
+            board.lists = lists//need to fix schema!!
+              res.send(handleResponse(action, board))
+          }).catch(error => {
+          return next(handleResponse(action, null, error))
         })
-          .catch(error => {
+        }).catch(error => {
           return next(handleResponse(action, null, error))
         })
     }
   },
-   getTasks:{ 
-    path: '/boards/:boardId/lists/:listId/tasks/',
+   getTasksOnLists: { 
+    path: '/boards/:boardId/lists/:listId/tasks',//Restful model for paths
     reqType: 'get',
     method(req, res, next){
-      let action = 'Get Tasks'
-      Lists.find({boardId: req.params.boardId})
-        .then(lists => {
-          lists.forEach(list=>{
-            Tasks.find({listId: list._id})
-            .then(tasks => {
-              res.send(handleResponse(action, lists))
-            })
-          })
-        })  
-        .catch(error => {
+      let action = 'Get tasks with Board Id'
+      Lists.findById(req.params.listId) 
+        .then(list => { 
+          Tasks.find({listId: req.params.listId})
+          .then(lists=>{
+            lists.tasks = tasks
+              res.send(handleResponse(action, list))
+          }).catch(error => {
           return next(handleResponse(action, null, error))
         })
-      }
+        }).catch(error => {
+          return next(handleResponse(action, null, error))
+        })
     }
+  },
+  getCommentsByTask:{
+    path: '/boards/:boardId/lists/:listId/tasks/:taskId/comments',
+    reqType: 'get',
+    method(req, res, next){
+      let action = "return comments asso. with a task"
+      Tasks.findById(req.params.taskId)
+      .then(task => {
+        Comments.find({taskId: req.params.taskId})
+          .then(comments => {
+            task.comments = comments
+              res.send(handleResponse(action, task))
+          }).catch(error => {
+          return next(handleResponse(action, null, error))
+          })
+      }).catch(error => {
+          return next(handleResponse(action, null, error))
+      })
+    }
+  },
+  getTasksAndAllComments: { //this function brakes convention b/c it does two things.
+    path: '/boards/:boardId/lists/:listId/tasks/comments',
+    reqType: 'get',
+    method(req, res, next){
+      let action = 'Get lists and all tasks with their comments'
+      Lists.findById(req.params.listId) 
+        .then(list => { 
+          Tasks.find({listId: req.params.listId})
+          .then(lists=>{
+            lists.tasks = tasks
+            Promise.all(tasks.map(task =>{//.map itterates over an array and returns an action for each item in the array, and builds a brand new array, does not change org array
+              return Comments.find({taskId: task._id})//comments.find always returns a promise, so we want to return our comments.find b/c we need to give our array...??
+              .then(comments =>{
+                task.comments = comments
+              }).catch(error => {
+                return next(handleResponse(action, null, error))
+            })
+            })).then(()=>{
+              res.send(handleResponse(action, list))
+            })
+              
+          }).catch(error => {
+          return next(handleResponse(action, null, error))
+        }).catch(error => {
+          return next(handleResponse(action, null, error))
+        })
+        }).catch(error => {
+          return next(handleResponse(action, null, error))
+        })
+    }
+  },
 }
 
 
