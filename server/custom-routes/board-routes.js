972a3fca2b551d1a350f8290a29e8b4400be8f94
Samantha Tanner <samz_shadow@yahoo.com>
2017-05-23 18:00:38 -0600

    completed getLists

--- server/custom-routes/board-routes.js
+++ server/custom-routes/board-routes.js
@@ -4,7 +4,7 @@ let Tasks = require('../models/task')
 let Comments = require('../models/comment')
 
 export default {
-  getAll:{
+  getAll:{ //do a getLists, getTasks,
     path: '/boards/:boardId/lists/:listId/tasks/:taskId/comments',
     reqType: 'get',
     method(req, res, next){
@@ -23,8 +23,27 @@ export default {
           return next(handleResponse(action, null, error))
         })
     }
+  },
+  getLists:{ //do a getTasks,
+    path: '/boards/:boardId/lists/',
+    reqType: 'get',
+    method(req, res, next){
+      let action = 'Get Lists'
+      Lists.find({boardId: req.params.boardId})
+        .then(lists => {
+          res.send(handleResponse(action, lists))
+        })
+          .catch(error => {
+          return next(handleResponse(action, null, error))
+        })
+    }
   }
 }
+
+
+
+
+
 function handleResponse(action, data, error) {
     var response = {
       action: action,
