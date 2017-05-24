3992926c41a023f5a4224110cb506fe5f998f77b
Samantha Tanner <samz_shadow@yahoo.com>
2017-05-24 15:38:45 -0600

    login.vue/register.vue

---
+++ www/src/components/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+          <div class="row"></div>
+          <div class="row">
+            <h1>Welcome</h1><h1>to bananaBerry</h1>
+          </div>
+          <div class="row">
+            <input type="text" placeholder="user name"><input type="password" placeholder="password"><button @click="">login</button>
+          </div>
+          <div class="row">
+            <h5>... register here!</h5> <!--if we use a berry in the logo, we can use a berry as a submit button, otherwise this will be hyper text-->
+          <div class="row"></div>
+          </div>
+        
+       
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'login',
+  data(){
+
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+.login{
+  background: #5989cc
+}
+</style>
---
+++ www/src/components/register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="reg">
+    <div class="container-fluid">
+      <div class="row"></div>
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+          <h2>login</h2>
+          <input class="text" placeholder="name">
+          <input class="email"  placeholder="email">
+          <input class="password" placeholder="set password">
+          <button @click="">submit!</button>
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+      <div class="row"></div>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "reg',
+  data(){
+
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+.login{
+  background: #5989cc
+}
+</style>
