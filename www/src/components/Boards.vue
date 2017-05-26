a7b847503a87f1576258f39ee06efbe153801d86
Samantha Tanner <samz_shadow@yahoo.com>
2017-05-26 12:14:01 -0600

    fixed it...

--- www/src/components/Boards.vue
+++ www/src/components/Boards.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>hello {{user.name}}</h1>
+  <div class="boards">
+    <h1></h1>
     <button @click="createBoard">Add Board</button>
     <ul>
       <li v-for="board in boards"><router-link :to="'/boards/'+board._id">{{board.name}}</router-link> <span @click="removeBoard(board)">x</span></li>
