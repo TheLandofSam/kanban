65363e256857c1c9e1ff8739ef99a32d98e356fe
Samantha Tanner <samz_shadow@yahoo.com>
2017-05-31 10:44:41 -0600

    schema adjustment

--- server/models/task.js
+++ server/models/task.js
@@ -5,7 +5,7 @@ let ObjectId = mongoose.Schema.ObjectId
 
 var schema = new mongoose.Schema({
 	name: { type: String, required: true },
-	description: { type: String },
+	//description removed in favor of one field: name
 	created: { type: Number, default: Date.now() },
 	// Relations
 	boardId: { type: ObjectId, ref: models.board.name, required: true },
